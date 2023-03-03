9adc740f9e751f741afabb937edbb9c8ce34eee8
info@harshaweb.com <info@harshaweb.com>
2023-03-03 19:49:49 +0530

    new 9

--- src/components/Body/OurProducts.jsx
+++ src/components/Body/OurProducts.jsx
@@ -29,7 +29,7 @@ function OurProducts() {
           <div className="mt-8 w-[80%] pt-24  m-auto flex justify-between">
             <div className="w-[40%] flex ">
               <div className="w-[85%]  text-center p-4 flex flex-col items-center">
-                <div className="flex btn p-0  w-[290px] h-[55px]   items-center justify-start  ">
+                <div className="flex btn p-0  w-[290px] h-[65px]   items-center justify-start  ">
                   <div className="h-[50px] w-[80px] flex items-center justify-center">
                     <img
                       src={build}
@@ -43,7 +43,7 @@ function OurProducts() {
                     </p>
                   </div>
                 </div>
-                <div className="flex btn w-[290px] h-[55px] mt-4  items-center justify-start p-0 ">
+                <div className="flex btn w-[290px] h-[65px] mt-4  items-center justify-start p-0 ">
                   <div className="h-[50px] w-[80px] flex items-center justify-center">
                     <img
                       src={location}
@@ -57,7 +57,7 @@ function OurProducts() {
                     </p>
                   </div>
                 </div>
-                <div className="flex btn w-[290px] h-[55px] mt-4  items-center justify-start p-0 ">
+                <div className="flex btn w-[290px] h-[65px] mt-4  items-center justify-start p-0 ">
                   <div className="h-[50px] w-[80px] flex items-center justify-center">
                     <img
                       src={AmenitiesIcon}
@@ -71,7 +71,7 @@ function OurProducts() {
                     </p>
                   </div>
                 </div>
-                <div className="flex btn w-[290px] h-[55px] mt-4  items-center justify-start p-0 ">
+                <div className="flex btn w-[290px] h-[65px] mt-4  items-center justify-start p-0 ">
                   <div className="h-[50px] w-[80px] flex items-center justify-center">
                     <img
                       src={Floorplan}
@@ -85,7 +85,7 @@ function OurProducts() {
                     </p>
                   </div>
                 </div>
-                <div className="flex btn w-[290px] h-[55px] mt-4  items-center justify-start p-0 ">
+                <div className="flex btn w-[290px] h-[65px] mt-4  items-center justify-start p-0 ">
                   <div className="h-[50px] w-[80px] flex items-center justify-center">
                     <img
                       src={salestool}
@@ -99,7 +99,7 @@ function OurProducts() {
                     </p>
                   </div>
                 </div>
-                <div className="flex btn w-[290px] h-[55px] mt-4  items-center justify-start p-0 ">
+                <div className="flex btn w-[290px] h-[65px] mt-4  items-center justify-start p-0 ">
                   <div className="h-[50px] w-[80px] flex items-center justify-center">
                     <img
                       src={Brochure}
@@ -113,7 +113,7 @@ function OurProducts() {
                     </p>
                   </div>
                 </div>
-                <div className="flex btn w-[290px] h-[55px] mt-4  items-center justify-start p-0 ">
+                <div className="flex btn w-[290px] h-[65px] mt-4  items-center justify-start p-0 ">
                   <div className="h-[50px] w-[80px] flex items-center justify-center">
                     <img
                       src={walkthrough}
@@ -130,7 +130,7 @@ function OurProducts() {
               </div>
               <div className="w-[2px] h-[70vh] bg-[#4614B9] border-[#4614B9]"></div>
             </div>
-            <div className="w-[50%]">
+            <div className="w-[60%]">
               <div className="flex flex-col items-center">
                 <img src={map} alt="" className="w-[100%] h-[100%]" />
               </div>
