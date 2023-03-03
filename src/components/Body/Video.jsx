65d05772c598efc416f1e1693bb9fda91588cdac
info@harshaweb.com <info@harshaweb.com>
2023-03-03 19:32:22 +0530

    new 2

--- src/components/Body/Video.jsx
+++ src/components/Body/Video.jsx
@@ -11,15 +11,15 @@ function Video() {
           <source src={video} type="video/mp4" />
         </video>
         {/*  */}
-        <div className="absolute mt-6 w-[80%] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <div className="absolute mt-16 w-[80%] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
           <h1 className="text-6xl font-extralight  text-white">
             Interactive 3D Solutions for Real Estate
           </h1>
           <div className="mt-6 flex items-center ">
-            <button className=" bg-[#4614B9]   text-white text-[17px] font-normal px-6 py-[10px] rounded-lg hover:bg-[#4614B9]  border-none mt-4">
+            <button className=" bg-[#4614B9]   text-white text-[17px] font-normal px-12 py-[10px] rounded-lg hover:bg-[#4614B9]  border-none mt-4">
               Book A Demo
             </button>
-            <button className=" bg-[#191919] flex items-center ml-6  text-white text-[17px] font-normal px-4 py-[10px] rounded-lg hover:bg-[#191919]  border-none mt-4">
+            <button className=" bg-[#191919] flex items-center ml-6  text-white text-[17px] font-normal px-6 py-[10px] rounded-lg hover:bg-[#191919]  border-none mt-4">
               <div>
                 <CiPlay1 className="text-[#fff] text-[20px] font-semibold mr-2  " />
               </div>
