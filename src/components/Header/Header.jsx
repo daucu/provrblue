dbbcff6967b6e5abaab3252ce546e8b189af24aa
info@harshaweb.com <info@harshaweb.com>
2023-03-14 19:02:28 +0530

    new

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import logo from "../Assets/Images/propvrwhite1.webp";
+import logo from "../Assets/Images/n_logo.png";
 import "../Assets/css/header.css";
 import { FiMenu } from "react-icons/fi";
 import main from "../Assets/Images/main.png";
@@ -11,11 +11,11 @@ function Header() {
 
   return (
     <>
-      <div className="containerHead bg-[#000000] p-8 fixed z-10 w-full">
+      <div className="containerHead bg-[#000000] p-6 fixed z-10 w-full">
         <div className="mainhead flex items-center  justify-between">
           <div className="w-[28%]">
             <Link to="/">
-              <img src={logo} alt="logo" className="w-[160px] m-auto" />
+              <img src={logo} alt="logo" className="w-[50px] m-auto" />
             </Link>
           </div>
           <div className="itemCont w-[65%] flex items-center justify-evenly ">
@@ -56,7 +56,13 @@ function Header() {
           <div className={active ? "fixed w-full p-4 left-0 top-0" : "hidden"}>
             <div className="bg-white p-[17px] rounded-md shadow-md text-black">
               <div className="flex justify-between">
-                <div className="font-bold">Logo</div>
+                <div className="font-bold">
+                  <div className="w-[28%]">
+                    <Link to="/">
+                      <img src={logo} alt="logo" className="w-[100px] m-auto" />
+                    </Link>
+                  </div>
+                </div>
                 <button
                   className="menuBtn p-2 rounded-md bg-[#191D24] hover:bg-[#4614B9]"
                   onClick={(e) => setActive(!active)}
@@ -64,17 +70,17 @@ function Header() {
                   <RxCross1 className="text-white text-[20px] cursor-pointer" />
                 </button>
               </div>
-              <div className="cursor-pointer ">Products</div>
+              <div className="cursor-pointer ml-4 mt-2 ">Products</div>
               <Link to="/metaverse">
-                <div className="cursor-pointer mt-2 ">Metaverse</div>
+                <div className="cursor-pointer mt-2 ml-4 ">Metaverse</div>
               </Link>
-              <div className="cursor-pointer mt-2 ">Spaces</div>
+              <div className="cursor-pointer mt-2 ml-4 ">Spaces</div>
               <Link to="/stories">
-                <div className="cursor-pointer mt-2 ">Success Stories</div>
+                <div className="cursor-pointer mt-2 ml-4 ">Success Stories</div>
               </Link>
-              <div className="cursor-pointer mt-2 ">Blog</div>
-              <div className="cursor-pointer mt-2 ">Contact Us</div>
-              <div className="cursor-pointer mt-2 ">About Us</div>
+              <div className="cursor-pointer mt-2 ml-4 ">Blog</div>
+              <div className="cursor-pointer mt-2 ml-4 ">Contact Us</div>
+              <div className="cursor-pointer mt-2 ml-4 ">About Us</div>
             </div>
           </div>
         </div>
