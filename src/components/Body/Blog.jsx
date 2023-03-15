a6e215c499d18adc7db88a2e9278984ed444da42
info@harshaweb.com <info@harshaweb.com>
2023-03-15 13:03:02 +0530

    blog

--- src/components/Body/Blog.jsx
+++ src/components/Body/Blog.jsx
@@ -1,158 +1,79 @@
 import React from "react";
-import  "../Assets/css/Blog.css"
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import ig from "../Assets/Images/gifs.gif";
+import er from "../Assets/Images/er.gif";
 function Blog() {
   return (
-    <div>
-      <>
-        <header>
-          <h1>Cool Articles</h1>
-        </header>
-        <div className="band">
-          <div className="item-1">
-            <a
-              href="https://design.tutsplus.com/articles/international-artist-feature-malaysia--cms-26852"
-              className="card"
-            >
-              <div
-                className="thumb"
-                style={{
-                  backgroundImage:
-                    "url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/flex-1.jpg)",
-                }}
-              />
-              <article>
-                <h1>International Artist Feature: Malaysia</h1>
-                <span>Mary Winkler</span>
-              </article>
-            </a>
+    <div className="bg-black">
+      <div className="md:pb-40 pb-28">
+        <Header />
+      </div>
+      <div className="w-full md:w-[95%] m-auto md:p-0 p-4">
+        <div className="text-white md:text-[35px] text-[22px] text-center">
+          Here Are Our Blogs
+        </div>
+      </div>
+      <div className="w-[95%] md:flex  m-auto pt-16  p-4">
+        <div className="md:w-1/2 w-full ">
+          <img
+            src={ig}
+            alt=""
+            className="md:w-[90%] w-full rounded-lg m-auto"
+          />
+        </div>
+        <div className="md:w-1/2 md:mt-0 mt-8 w-full ">
+          <div className=" font-semibold md:text-[25px] underline decoration-[#4614B9]  text-[white] text-[20px] ">
+            Our Achievement
           </div>
-          <div className="item-2">
-            <a
-              href="https://webdesign.tutsplus.com/articles/how-to-conduct-remote-usability-testing--cms-27045"
-              className="card"
-            >
-              <div
-                className="thumb"
-                style={{
-                  backgroundImage:
-                    "url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/users-2.png)",
-                }}
-              />
-              <article>
-                <h1>How to Conduct Remote Usability Testing</h1>
-                <span>Harry Brignull</span>
-              </article>
-            </a>
+          <div className=" font-extralight text-justify text-white mt-4 md:text-[23px] text-[18px] ">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad vel
+            illo quia voluptatibus recusandae distinctio provident ipsum maxime,
+            corporis fuga, quibusdam deleniti libero laboriosam nam sapiente.
+            Iure provident maiores sit. Lorem ipsum dolor sit, amet consectetur
+            adipisicing elit. Saepe ducimus harum minus quae quod voluptatum, et
+            vero eveniet fugit incidunt explicabo nemo odio nulla aspernatur
+            aliquam natus dolore dolorum quidem.
           </div>
-          <div className="item-3">
-            <a
-              href="https://design.tutsplus.com/articles/envato-tuts-community-challenge-created-by-you-july-edition--cms-26724"
-              className="card"
-            >
-              <div
-                className="thumb"
-                style={{
-                  backgroundImage:
-                    "url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/flex-5.jpg)",
-                }}
-              />
-              <article>
-                <h1>Created by You, July Edition</h1>
-                <p>
-                  Welcome to our monthly feature of fantastic tutorial results
-                  created by you, the Envato Tuts+ community!{" "}
-                </p>
-                <span>Melody Nieves</span>
-              </article>
-            </a>
-          </div>
-          <div className="item-4">
-            <a
-              href="https://webdesign.tutsplus.com/tutorials/how-to-code-a-scrolling-alien-lander-website--cms-26826"
-              className="card"
-            >
-              <div
-                className="thumb"
-                style={{
-                  backgroundImage:
-                    "url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/landing.png)",
-                }}
-              />
-              <article>
-                <h1>How to Code a Scrolling “Alien Lander” Website</h1>
-                <p>
-                  We’ll be putting things together so that as you scroll down
-                  from the top of the page you’ll see an “Alien Lander” making
-                  its way to touch down.
-                </p>
-                <span>Kezz Bracey</span>
-              </article>
-            </a>
-          </div>
-          <div className="item-5">
-            <a
-              href="https://design.tutsplus.com/tutorials/stranger-things-inspired-text-effect--cms-27139"
-              className="card"
-            >
-              <div
-                className="thumb"
-                style={{
-                  backgroundImage:
-                    "url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/strange.jpg)",
-                }}
-              />
-              <article>
-                <h1>
-                  How to Create a “Stranger Things” Text Effect in Adobe
-                  Photoshop
-                </h1>
-                <span>Rose</span>
-              </article>
-            </a>
-          </div>
-          <div className="item-6">
-            <a
-              href="https://photography.tutsplus.com/articles/5-inspirational-business-portraits-and-how-to-make-your-own--cms-27338"
-              className="card"
-            >
-              <div
-                className="thumb"
-                style={{
-                  backgroundImage:
-                    "url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/flor.jpg)",
-                }}
-              />
-              <article>
-                <h1>
-                  5 Inspirational Business Portraits and How to Make Your Own
-                </h1>
-                <span>Marie Gardiner</span>
-              </article>
-            </a>
-          </div>
-          <div className="item-7">
-            <a
-              href="https://webdesign.tutsplus.com/articles/notes-from-behind-the-firewall-the-state-of-web-design-in-china--cms-22281"
-              className="card"
-            >
-              <div
-                className="thumb"
-                style={{
-                  backgroundImage:
-                    "url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/china.png)",
-                }}
-              />
-              <article>
-                <h1>
-                  Notes From Behind the Firewall: The State of Web Design in
-                  China
-                </h1>
-                <span>Kendra Schaefer</span>
-              </article>
-            </a>
+          <div className=" font-semibold   text-[20px] mt-6 ">
+            By : John Doe
           </div>
         </div>
-      </>
+      </div>
+      <div className="w-full md:w-[95%] m-auto md:mt-24 mt-8 md:p-0 p-4">
+        <div className="text-white md:text-[35px] text-[22px] text-center">
+       Our Tasks
+        </div>
+      </div>
+      <div className="w-[95%] md:flex   m-auto pt-24  p-4">
+        <div className="md:w-1/2 md:mt-0 mt-8 w-full ">
+          <div className=" font-semibold md:text-[25px] underline decoration-[#4614B9]  text-[white] text-[20px] ">
+            Our Achievement
+          </div>
+          <div className=" font-extralight text-justify text-white mt-4 md:text-[23px] text-[18px] ">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad vel
+            illo quia voluptatibus recusandae distinctio provident ipsum maxime,
+            corporis fuga, quibusdam deleniti libero laboriosam nam sapiente.
+            Iure provident maiores sit. Lorem ipsum dolor sit, amet consectetur
+            adipisicing elit. Saepe ducimus harum minus quae quod voluptatum, et
+            vero eveniet fugit incidunt explicabo nemo odio nulla aspernatur
+            aliquam natus dolore dolorum quidem.
+          </div>
+          <div className=" font-semibold   text-[20px] mt-6 ">
+            By : John Doe
+          </div>
+        </div>
+        <div className="md:w-1/2 w-full ">
+          <img
+            src={er}
+            alt=""
+            className="md:w-[90%] w-full rounded-lg m-auto"
+          />
+        </div>
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 }
