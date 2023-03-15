0d560baaa81a56c4013e79500e6689cda7970616
info@harshaweb.com <info@harshaweb.com>
2023-03-15 14:55:09 +0530

    new

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import logo from "../Assets/Images/propvrwhite1.webp";
+// import logo from "../Assets/Images/propvrwhite1.webp";
 import { BsInstagram, BsLinkedin, BsTwitter, BsYoutube } from "react-icons/bs";
+import logo from "../Assets/Images/n_logo.png";
 
 function Footer() {
   return (
     <div className="md:mt-24 mt-52">
       <div className="md:flex md:w-[80%] w-[88%] m-auto  ">
         <div className="md:w-[30%] w-full">
-          <div className="w-[150px]">
-            <img src={logo} />
+          <div className="w-[150px] flex items-center">
+            <img src={logo} className="w-1/3" />
+            <div className="font-semibold text-white ml-2 text-[20px]">Neuractory</div>
           </div>
           <div className="flex mt-4 items-center ">
             <div className="m-2">
@@ -44,7 +46,9 @@ function Footer() {
             </div>
           </div>
           <div>
-            <h1 className="text-[#F2F2F2] md:mt-0 mt-8 text-xl font-bold">Company</h1>
+            <h1 className="text-[#F2F2F2] md:mt-0 mt-8 text-xl font-bold">
+              Company
+            </h1>
             <div className="mt-4">
               <p className="text-[#F2F2F2] text-sm mt-4">About Us</p>
               <p className="text-[#F2F2F2] text-sm mt-4">Careers</p>
@@ -53,7 +57,9 @@ function Footer() {
             </div>
           </div>
           <div>
-            <h1 className="text-[#F2F2F2] md:mt-0 mt-8 text-xl font-bold">Company</h1>
+            <h1 className="text-[#F2F2F2] md:mt-0 mt-8 text-xl font-bold">
+              Company
+            </h1>
             <div className="mt-4">
               <p className="text-[#F2F2F2] text-sm mt-4">About Us</p>
               <p className="text-[#F2F2F2] text-sm mt-4">Careers</p>
@@ -65,7 +71,7 @@ function Footer() {
       </div>
       <div className="mt-8   w-[80%] m-auto border-b-2  bg-white"></div>
       <div className="mt-8 w-[80%] m-auto font-bold text-center pb-8">
-      Copyright © 2016 Propvr.
+        Copyright © 2016 Propvr.
       </div>
     </div>
   );
