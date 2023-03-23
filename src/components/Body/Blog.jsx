8427da58d4f56924c0c1ffddacd7e5d15e01b836
info@harshaweb.com <info@harshaweb.com>
2023-03-23 11:54:47 +0530

    new

--- src/components/Body/Blog.jsx
+++ src/components/Body/Blog.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect } from "react";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
-import ig from "../Assets/Images/gifs.gif";
-import er from "../Assets/Images/er.gif";
+import { AiOutlineRight, AiOutlineSearch } from "react-icons/ai";
+import { BiFilterAlt } from "react-icons/bi";
+
+import blur1 from "../Assets/Images/blur.webp";
+import blur2 from "../Assets/Images/blur2.webp";
+import blur3 from "../Assets/Images/blur3.webp";
 import Messages from "./Messages";
 function Blog() {
   const scrollToTop = () => {
@@ -19,58 +23,76 @@ function Blog() {
       <div className="md:pb-40 pb-28">
         <Header />
       </div>
-      <div className="w-full md:w-[90%] m-auto md:p-0 p-4">
-        <div className="text-white md:text-[35px] text-[22px] text-center">
-          Here Are Our Blogs
-        </div>
-      </div>
-      <div className="w-[90%] md:flex items-center  m-auto pt-16  p-4">
-        <div className="md:w-1/2 w-full ">
-          <img
-            src={ig}
-            alt=""
-            className="md:w-[90%] w-full rounded-lg m-auto"
-          />
-        </div>
-        <div className="md:w-1/2 md:mt-0 mt-8 w-full ">
-          <div className=" font-semibold md:text-[25px] underline decoration-[#4614B9]  text-[white] text-[20px] ">
-            Design Your Own Reality
+
+      <div className="md:px-36 p-8  top-[-50px] relative   bg-[#47454526] ">
+        <div className="md:flex items-center justify-between">
+          <div className="md:w-[40%] flex items-center  ">
+            <div className="font-semibold text-white text-[18px]">Home</div>
+            <div className="font-semibold text-white text-[16px] ml-2 mr-2">
+              <AiOutlineRight />
+            </div>
+            <div className="font-semibold text-[#391091] text-[18px]">
+              All Post
+            </div>
           </div>
-          <div className=" font-extralight text-justify text-white mt-4 md:text-[23px] text-[18px] ">
-            Step into an immersive alternate reality with the Metaverse BE THE
-            FIRST TO Elevate Your Project Experience with a Transformation!
-            Metaverse is more than just buzz, it's a digital revolution!
-            Companies worldwide are investing millions to transform, so why not
-            join the trend? Let us help with your Metaverse transformation!
-          </div>
-        </div>
-      </div>
-      <div className="w-full md:w-[95%] m-auto md:mt-24 mt-8 md:p-0 p-4">
-        <div className="text-white md:text-[35px] text-[22px] text-center">
-          Testimonials
-        </div>
-      </div>
-      <div className="w-[90%] md:flex  items-center  m-auto pt-24  p-4">
-        <div className="md:w-1/2 w-full ">
-          <img
-            src={er}
-            alt=""
-            className="md:w-[90%] w-full rounded-lg m-auto"
-          />
-        </div>
-        <div className="md:w-1/2 md:mt-0 mt-8 w-full ">
-          <div className=" font-semibold md:text-[25px] underline decoration-[#4614B9]  text-[white] text-[20px] ">
-            Get the Inside Scoop
-          </div>
-          <div className=" font-extralight text-justify text-white mt-4 md:text-[23px] text-[18px] ">
-            Neuractory exceeded our expectations with their AR/VR development
-            for our commercial project. The complete exterior and interior views
-            were absolutely stunning and helped us to better visualize our
-            vision for the space. Their team was professional, responsive, and
-            delivered the project on time. We highly recommend Neuractory for
-            any AR/VR needs!
+          <div className="w-[50%] flex items-center justify-evenly ">
+            <div className="flex bg-[#191919] hover:border-white border border-[#191919] p-[10px] rounded-md">
+              <input
+                className="bg-[#191919]  outline-none  text-white font-semibold text-[20px]"
+                placeholder="Search Here.."
+              />
+              <button>
+                <AiOutlineSearch size={25} />
+              </button>
+            </div>
+            <div>
+              <button className="flex items-center btn text-white font-semibold">
+                <div>
+                  <BiFilterAlt size={20} />
+                </div>
+                <div className="text-[20px] ml-2">Filter</div>
+              </button>
+            </div>
           </div>
         </div>
+        <div className="md:mt-20">
+          <div className="text-white text-center font-bold text-[30px]">
+            All Post
+          </div>
+          <div className="text-white text-center font-semibold mt-4 text-[20px]">
+            Unlocking the potential of Digital twins, Metaverse and 3d
+            visualistations
+          </div>
+          <div className="flex mt-12 justify-evenly items-center">
+            <div className="">
+              <button className="btn bg-[#191919] hover:bg-[#4614B9] text-white font-semibold text-[17px]">
+                All
+              </button>
+            </div>
+            <div className="">
+              <button className="btn bg-[#191919] hover:bg-[#4614B9] text-white font-semibold text-[17px]">
+                Virtual Reality
+              </button>
+            </div>
+            <div className="">
+              <button className="btn bg-[#191919] hover:bg-[#4614B9] text-white font-semibold text-[17px]">
+                Metaverse
+              </button>
+            </div>
+            <div className="">
+              <button className="btn bg-[#191919] hover:bg-[#4614B9] text-white font-semibold text-[17px]">
+                Property Branding
+              </button>
+            </div>
+          </div>
+        </div>
+        <div
+          className="h-[60vh] blur-[100px]	"
+          style={{
+            backgroundImage: `url(${blur1})`,
+            backgroundRepeat: "no-repeat",
+          }}
+        ></div>
       </div>
       <div>
         <Messages />
