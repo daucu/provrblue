c1e60214a2eb945d50fc5b74c51b364c8dc99675
info@harshaweb.com <info@harshaweb.com>
2023-03-23 15:07:29 +0530

    new

--- src/components/Body/Blog.jsx
+++ src/components/Body/Blog.jsx
@@ -3,7 +3,7 @@ import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import { AiOutlineRight, AiOutlineSearch } from "react-icons/ai";
 import { BiFilterAlt } from "react-icons/bi";
-
+import "../Assets/css/Blog.css";
 import blur1 from "../Assets/Images/blur.webp";
 import blur2 from "../Assets/Images/blur2.webp";
 import blur3 from "../Assets/Images/blur3.webp";
@@ -30,9 +30,9 @@ function Blog() {
         <Header />
       </div>
 
-      <div className="md:px-36 p-8  top-[-50px] relative   bg-[#47454526] ">
+      <div className="md:px-36 py-16 md:p-0 p-4 md:mt-0 mt-8  top-[-90px] relative   bg-[#47454526] ">
         <div className="md:flex items-center justify-between">
-          <div className="md:w-[40%] flex items-center  ">
+          <div className="leftcont md:w-[40%] flex items-center  ">
             <div className="font-semibold text-white text-[18px]">Home</div>
             <div className="font-semibold text-white text-[16px] ml-2 mr-2">
               <AiOutlineRight />
@@ -41,17 +41,19 @@ function Blog() {
               All Post
             </div>
           </div>
-          <div className="w-[50%] flex items-center justify-evenly ">
-            <div className="flex bg-[#191919] hover:border-white border border-[#191919] p-[10px] rounded-md">
+          <div className="rightcont  md:flex items-center  ">
+            <div className="flex bg-[#191919] justify-between hover:border-white border border-[#191919] p-[10px] rounded-md">
               <input
                 className="bg-[#191919]  outline-none  text-white font-semibold text-[20px]"
                 placeholder="Search Here.."
               />
-              <button>
-                <AiOutlineSearch size={25} />
-              </button>
+              <div>
+                <button>
+                  <AiOutlineSearch size={25} />
+                </button>
+              </div>
             </div>
-            <div>
+            <div className="md:mt-0 mt-4">
               <button className="flex items-center btn text-white font-semibold">
                 <div>
                   <BiFilterAlt size={20} />
@@ -61,7 +63,7 @@ function Blog() {
             </div>
           </div>
         </div>
-        <div className="md:mt-20">
+        <div className="md:mt-20 mt-12">
           <div className="text-white text-center font-bold text-[30px]">
             All Post
           </div>
@@ -69,23 +71,23 @@ function Blog() {
             Unlocking the potential of Digital twins, Metaverse and 3d
             visualistations
           </div>
-          <div className="flex mt-12 justify-evenly items-center">
-            <div className="">
+          <div className="flex mt-12 justify-between sm:justify-evenly items-center overflow-x-scroll scroll">
+            <div className="shrink-0 mx-2">
               <button className="btn bg-[#191919] hover:bg-[#4614B9] text-white font-semibold text-[17px]">
                 All
               </button>
             </div>
-            <div className="">
+            <div className="shrink-0 mx-2">
               <button className="btn bg-[#191919] hover:bg-[#4614B9] text-white font-semibold text-[17px]">
                 Virtual Reality
               </button>
             </div>
-            <div className="">
+            <div className="shrink-0 mx-2">
               <button className="btn bg-[#191919] hover:bg-[#4614B9] text-white font-semibold text-[17px]">
                 Metaverse
               </button>
             </div>
-            <div className="">
+            <div className="shrink-0 mx-2">
               <button className="btn bg-[#191919] hover:bg-[#4614B9] text-white font-semibold text-[17px]">
                 Property Branding
               </button>
@@ -94,16 +96,16 @@ function Blog() {
         </div>
         <div className=" relative">
           <div
-            className="min-h-[350vh] z-1 blur-[100px]	"
+            className="min-h-[100vh] z-1 blur-[100px]	"
             style={{
               backgroundImage: `url(${blur1})`,
               backgroundRepeat: "no-repeat",
             }}
           ></div>
           <div className="w-full absolute top-0 ">
-            <div className=" py-20   absolute  flex flex-col items-center lg:flex-row lg:items-stretch justify-center flex-wrap top-0">
-              <div className="w-[280px] lg:w-1/2 xl:w-1/4">
-                <div className="m-3 bg-transparent p-[10px] border-2 h-full  border-r-[#471E6F] border-l-[#471E6F] border-t-[#205677] border-b-[#205677] flex flex-col justify-between">
+            <div className=" py-20 flex flex-col items-center lg:flex-row lg:items-stretch justify-center flex-wrap top-0">
+              <div className="cursor-pointer m-auto max-w-[300px] lg:w-1/2 xl:w-1/4">
+                <div className="m-3 bg-transparent p-[10px] border-2 min-h-full  border-r-[#471E6F] border-l-[#471E6F] border-t-[#205677] border-b-[#205677] flex flex-col justify-between">
                   <div className="h-[250px]">
                     <img src={f1} alt="" className="h-[100%]" />
                   </div>
@@ -121,11 +123,10 @@ function Blog() {
                   </div>
                 </div>
               </div>
-
-              <div className="w-[280px] lg:w-1/2 xl:w-1/4">
-                <div className="m-3 bg-transparent  p-[10px] border-2 h-full    border-r-[#471E6F] border-l-[#471E6F] border-t-[#205677] border-b-[#205677] flex flex-col justify-between">
+              <div className="cursor-pointer m-auto max-w-[300px] lg:w-1/2 xl:w-1/4">
+                <div className="m-3 bg-transparent p-[10px] border-2 min-h-full  border-r-[#471E6F] border-l-[#471E6F] border-t-[#205677] border-b-[#205677] flex flex-col justify-between">
                   <div className="h-[250px]">
-                    <img src={f1} alt="" className="h-[100%]" />
+                    <img src={f2} alt="" className="h-[100%]" />
                   </div>
                   <div className="grow">
                     <div className="flex items-center  mt-8">
@@ -134,17 +135,36 @@ function Blog() {
                         Metaverse
                       </div>
                     </div>
-                    <div className=" text-white py-8 font-bold text-[20px]">
-                      The Metaverse: An Introduction to the Next Dimension
-                    </div>
+                  </div>
+                  <div className=" text-white py-8 font-bold text-[20px]">
+                    The Metaverse: An Introduction to the Next Dimension
+                    designing and developing the metaverse
                   </div>
                 </div>
               </div>
-
-              <div className="w-[280px] lg:w-1/2 xl:w-1/4">
-                <div className="m-3 bg-transparent p-[10px] border-2 h-full   border-r-[#471E6F] border-l-[#471E6F] border-t-[#205677] border-b-[#205677] flex flex-col justify-between">
+              <div className="cursor-pointer m-auto max-w-[300px] lg:w-1/2 xl:w-1/4">
+                <div className="m-3 bg-transparent p-[10px] border-2 min-h-full  border-r-[#471E6F] border-l-[#471E6F] border-t-[#205677] border-b-[#205677] flex flex-col justify-between">
                   <div className="h-[250px]">
-                    <img src={f1} alt="" className="h-[100%]" />
+                    <img src={f3} alt="" className="h-[100%]" />
+                  </div>
+                  <div className="grow">
+                    <div className="flex items-center  mt-8">
+                      <div className="w-[60px] h-[3px] bg-[#4614B9]"></div>
+                      <div className="font-normal ml-6 text-white text-[18px]">
+                        Property Branding
+                      </div>
+                    </div>
+                  </div>
+                  <div className=" text-white py-8 font-bold text-[20px]">
+                    Why 3D Floorplans Are a Game-Changer for Construction
+                    Business Agents?
+                  </div>
+                </div>
+              </div>
+              <div className="cursor-pointer m-auto max-w-[300px] lg:w-1/2 xl:w-1/4">
+                <div className="m-3 bg-transparent p-[10px] border-2 min-h-full  border-r-[#471E6F] border-l-[#471E6F] border-t-[#205677] border-b-[#205677] flex flex-col justify-between">
+                  <div className="h-[250px] ">
+                    <img src={f4} alt="" className="h-[100%]" />
                   </div>
                   <div className="grow">
                     <div className="flex items-center  mt-8">
@@ -157,24 +177,6 @@ function Blog() {
                   <div className=" text-white py-8 font-bold text-[20px]">
                     Elevate Your Real Estate Game with 360° Aerial Panoramas in
                     Virtual Property Tours
-                  </div>
-                </div>
-              </div>
-              <div className="w-[280px] lg:w-1/2 xl:w-1/4">
-                <div className="m-3 bg-transparent h-full  p-[10px] border-2    border-r-[#471E6F] border-l-[#471E6F] border-t-[#205677] border-b-[#205677] flex flex-col justify-between">
-                  <div className="h-[250px]">
-                    <img src={f1} alt="" className="h-[100%]" />
-                  </div>
-                  <div className="grow">
-                    <div className="flex items-center  mt-8">
-                      <div className="w-[60px] h-[3px] bg-[#4614B9]"></div>
-                      <div className="font-normal ml-6 text-white text-[18px]">
-                        Metaverse
-                      </div>
-                    </div>
-                    <div className=" text-white py-8 font-bold text-[20px]">
-                      The Metaverse: An Introduction to the Next Dimension
-                    </div>
                   </div>
                 </div>
               </div>
