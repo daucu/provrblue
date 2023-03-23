2beb274c3ae3f7f3d1c7d529428abbc29733db83
info@harshaweb.com <info@harshaweb.com>
2023-03-23 16:37:06 +0530

    new

--- src/components/Header/BlogHeader.jsx
+++ src/components/Header/BlogHeader.jsx
@@ -1,18 +1,31 @@
 import React from "react";
+import "../Assets/css/BlogHeader.css";
+import { BiHome } from "react-icons/bi";
+import { GrMenu } from "react-icons/gr";
 
 function BlogHeader() {
   return (
     <div className="bg-white border-b-2 p-4 shadow-md">
-      <div className="flex items-end justify-end">
-        <div className="w-1/2 flex items-end justify-end">
-          <div className="text-[20px] text-blue-500 w-[33%]">Home</div>
-          <div className="text-[20px] text-blue-500 w-[33%]">
+      <div className="maincont items-end justify-end">
+        <div className="mainitem  flex items-end ">
+          <div className="itemcont text-[20px] text-blue-500 ">Home</div>
+          <div className="itemcont text-[20px] text-blue-500 ">
             Virtual Reality
           </div>
-          <div className="text-[20px] text-blue-500 w-[33%]">Metaverse</div>
-          <div className="text-[20px] text-blue-500 w-[33%]">
+          <div className="itemcont text-[20px] text-blue-500 ">Metaverse</div>
+          <div className="itemcont text-[20px] text-blue-500 ">
             Property Branding
           </div>
+        </div>
+      </div>
+      <div className="menubtn">
+        <div className="mr-6">
+          <BiHome size={25} />
+        </div>
+        <div>
+          <button>
+            <GrMenu size={25} />
+          </button>
         </div>
       </div>
     </div>
